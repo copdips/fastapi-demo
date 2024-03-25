0e34fb8699678d275bfc7bfa8dabed2716afc130
Xiang ZHU <xiang.zhu@outlook.com>
2024-03-25 20:56:25 +0100

    minor router refactor

--- app/core/router.py
+++ app/core/router.py
@@ -5,7 +5,7 @@ import types
 from fastapi import APIRouter, FastAPI
 
 
-def add_child_router(parent_router: APIRouter, child_router_module: types.ModuleType):
+def _add_child_router(parent_router: APIRouter, child_router_module: types.ModuleType):
     parent_router.include_router(
         child_router_module.router,
         prefix=f"/{child_router_module.endpoint_name}",
@@ -13,22 +13,22 @@ def add_child_router(parent_router: APIRouter, child_router_module: types.Module
     )
 
 
-def list_submodules(package):
-    submodules = []
-    for _, name, ispkg in pkgutil.iter_modules(
+def _list_submodules(package: types.ModuleType) -> list[types.ModuleType]:
+    submodules: list[types.ModuleType] = []
+    for _, name, is_pkg in pkgutil.iter_modules(
         package.__path__,
-        package.__name__ + ".",
+        f"{package.__name__}.",
     ):
-        if not ispkg:
+        if not is_pkg:
             module = importlib.import_module(name)
             submodules.append(module)
     return submodules
 
 
 def register_routers(app: FastAPI, routers_parent_module: types.ModuleType):
-    routers_module = list_submodules(routers_parent_module)
+    routers_module = _list_submodules(routers_parent_module)
     [
-        add_child_router(routers_parent_module.router, router)
+        _add_child_router(routers_parent_module.router, router)
         for router in routers_module
     ]
     app.include_router(
