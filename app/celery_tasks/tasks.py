31c8bc7f839e5acf730b619e8e9dc75e9416cf5f
Xiang ZHU <xiang.zhu@outlook.com>
2024-04-19 23:58:08 +0200

    add two celery workers for 2 queues low and high

--- app/celery_tasks/tasks.py
+++ app/celery_tasks/tasks.py
@@ -4,8 +4,17 @@ from app.core.logging import get_logger
 logger = get_logger()
 
 
-@celery_app.task
-def celery_task_demo():
+@celery_app.task(queue="low")
+def celery_task_demo_queue_low():
+    import time
+
+    time.sleep(2)
+    logger.info("Finished task: celery_task_demo")
+    return "okok"
+
+
+@celery_app.task(queue="high")
+def celery_task_demo_queue_high():
     import time
 
     time.sleep(2)
