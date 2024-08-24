02d3a0f91eb6dca15baf1a367508f679cd88af00
Xiang ZHU <xiang.zhu@outlook.com>
2024-08-24 22:57:07 +0200

    add multiple outputs fro eralchemy

--- tools/sql_to_erd/eralchemy2_demo/eralchemy2_demo.sh
+++ tools/sql_to_erd/eralchemy2_demo/eralchemy2_demo.sh
@@ -3,3 +3,6 @@
 # sudo apt install graphviz libgraphviz-dev
 
 eralchemy2 -i sqlite:///db.sqlite3 -o ./tools/sql_to_erd/eralchemy2_demo/eralchemy2.pdf
+eralchemy2 -i sqlite:///db.sqlite3 -o ./tools/sql_to_erd/eralchemy2_demo/eralchemy2.png
+eralchemy2 -i sqlite:///db.sqlite3 -o ./tools/sql_to_erd/eralchemy2_demo/eralchemy2.svg
+eralchemy2 -i sqlite:///db.sqlite3 -o ./tools/sql_to_erd/eralchemy2_demo/eralchemy2.md
