82066e3115db5d93e20e50e54f1a4fcc94895306
Xiang ZHU <xiang.zhu@outlook.com>
2025-10-18 22:50:10 +0200

    use new pytest-asyncio conf

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,19 +1,9 @@
-import asyncio
 from collections.abc import Generator
 
 import pytest
 from fastapi.testclient import TestClient
 
 from app.main import app
-
-
-@pytest.fixture(scope="session")
-def event_loop():
-    # https://github.com/pytest-dev/pytest-asyncio/issues/706#issuecomment-1838771298
-    # https://github.com/pytest-dev/pytest-asyncio/issues/706#issuecomment-2282780197
-    loop = asyncio.get_event_loop()
-    yield loop
-    loop.close()
 
 
 @pytest.fixture(scope="module")
